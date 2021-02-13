2c3e4b74f621844d0ba1c0c02ce000703564262a
brenosekiro <b88660501@gmail.com>
2021-02-12 22:13:05 -0300

    tab

--- lib/data/product_data.dart
+++ lib/data/product_data.dart
@@ -17,7 +17,7 @@ class ProductData {
     id = snapshot.id;
     title = snapshot.data()['title'];
     description = snapshot.data()['description'];
-    price = snapshot.data()['price'];
+    price = snapshot.data()['price'] + 0.0;
     images = snapshot.data()['images'];
     sizes = snapshot.data()['sizes'];
   }
