c17be1f1fa676ea971b842811d008aab5f06d6f0
brenosekiro <b88660501@gmail.com>
2021-02-16 15:45:40 -0300

    real time status

--- lib/widgets/order_tile.dart
+++ lib/widgets/order_tile.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderTile extends StatelessWidget {
@@ -8,8 +11,101 @@ class OrderTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: StreamBuilder<DocumentSnapshot>(
+          builder: (context, snapshot) {
+            if(!snapshot.hasData)
+              return Center(child: CircularProgressIndicator());
+            else {
+
+              int status = snapshot.data.data()['status'];
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Código do pedido: ${snapshot.data.id}",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                  SizedBox(height: 4.0),
+                  Text(_buildProductsText(snapshot.data)),
+                  SizedBox(height: 4.0),
+                  Text("Status do pedido",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                  SizedBox(height: 4.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildCircle("1", "Preaparação", status, 1),
+                      Container(
+                        height: 1.0,
+                        width: 40.0,
+                        color: Colors.grey[500],
+                      ),
+                      _buildCircle("2", "Transporte", status, 2),
+                      Container(
+                        height: 1.0,
+                        width: 40.0,
+                        color: Colors.grey[500],
+                      ),
+                      _buildCircle("3", "Entrega", status, 3)
+                    ]
+                  )
+                ],
+              );
+            }
+          },
+          stream: FirebaseFirestore.instance.collection('orders').doc(orderId).snapshots(),
+        ),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0)
     );
+  }
+
+  String _buildProductsText(DocumentSnapshot snapshot) {
+    String text = 'Descrição:\n';
+    for(LinkedHashMap p in snapshot.data()['products']) {
+      text += "${p['quantity']} x ${p['product']['title']} (R\$ ${p['product']['price'].toStringAsFixed(2)}) \n";
+      text += "Total: R\$ ${snapshot.data()['totalPrice'].toStringAsFixed(2)}";
+    }
+
+    return text;
+  }
+
+  Widget _buildCircle(String title, String subtitle, int status, int thisStatus) {
+
+    Color backColor;
+    Widget child;
+
+    if(status < thisStatus) {
+      backColor = Colors.grey[500];
+      child = Text(title, style: TextStyle(color: Colors.white));
+    } else if(status == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: [
+          Text(title, style: TextStyle(color: Colors.white)),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white)
+          )
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = Icon(Icons.check, color: Colors.white);
+    }
+
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: backColor,
+          child: child
+        ),
+        Text(subtitle)
+      ],
+    );
+
   }
 }
